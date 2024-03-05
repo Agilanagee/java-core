4be8871a4c524f2407836f99065b6509afa0bd2e
Agilanagee <113231297+Agilanagee@users.noreply.github.com>
2024-03-05 08:37:50 +0530

    Add files via upload

---
+++ com.learning/day02/D02P16.java
@@ -0,0 +1,24 @@
+package day02;
+
+import java.util.Scanner;
+
+public class D02P16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+
+        int fib1 = 0, fib2 = 1;
+
+        System.out.println("Fibonacci series up to " + n + "th number:");
+        System.out.print(fib1 + " ");
+
+        for (int i = 1; i < n; i++) {
+            System.out.print(fib2 + " ");
+            int nextFib = fib1 + fib2;
+            fib1 = fib2;
+            fib2 = nextFib;
+        }  
+    }
+}
