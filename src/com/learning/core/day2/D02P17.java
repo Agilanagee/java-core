c51d77c34c2fc3b63f1fbf7e16b7b006281a8ef5
Agilanagee <113231297+Agilanagee@users.noreply.github.com>
2024-03-11 16:20:04 +0530

    Add files via upload

---
+++ src/com/learning/core/day2/D02P17.java
@@ -0,0 +1,32 @@
+package com.learning.core.day2;
+
+import java.util.Scanner;
+
+public class D02P17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value of n: ");
+        int n = sc.nextInt();
+        System.out.println("Prime numbers from 1 to " + n + ":");
+        printPrimeNumbers(n);
+    }
+    public static void printPrimeNumbers(int n) {
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
