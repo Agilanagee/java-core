8f83b6c799c7c23d201e62cb734312245144dc61
Agilanagee <113231297+Agilanagee@users.noreply.github.com>
2024-03-11 16:21:49 +0530

    Add files via upload

---
+++ src/com/learning/core/day3/D03P01.java
@@ -0,0 +1,25 @@
+package com.learning.core.day3;
+
+import java.util.Scanner;
+
+public class D03P01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        int length = input.length();
+        System.out.println("Length of string is : " + length);
+        String uppercaseString = input.toUpperCase();
+        System.out.println("Uppercase string: " + uppercaseString);
+        boolean isPalindrome = isPalindrome(input);
+        if (isPalindrome) {
+            System.out.println("It string is a palindrome");
+        } else {
+            System.out.println("It string is not a palindrome");
+        }
+    }
+    public static boolean isPalindrome(String str) {
+        StringBuilder reversed = new StringBuilder(str).reverse();
+        return str.equalsIgnoreCase(reversed.toString());
+    }
+}
---
+++ src/com/learning/core/day3/D03P02.java
@@ -0,0 +1,32 @@
+package com.learning.core.day3;
+
+import java.util.Scanner;
+
+public class D03P02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbersArray = {8, 13, 15, 28, 43};
+        System.out.print("Enter first number (between 1 and 40): ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter second number (between 1 and 40): ");
+        int num2 = scanner.nextInt();
+        boolean foundNum1 = false;
+        boolean foundNum2 = false;
+
+        for (int num : numbersArray) {
+            if (num == num1) {
+                foundNum1 = true;
+            }
+            if (num == num2) {
+                foundNum2 = true;
+            }
+        }
+        if (foundNum1 && foundNum2) {
+            System.out.println("Bingo");
+        } 
+        else {
+            System.out.println("Numbers not found together in the array.");
+        }
+    }
+}
+
