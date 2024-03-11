113c03aaab20c3de3574e21622fafb09804dcf48
Agilanagee <113231297+Agilanagee@users.noreply.github.com>
2024-03-11 16:19:05 +0530

    Add files via upload

---
+++ src/com/learning/core/day2/D02P08.java
@@ -0,0 +1,23 @@
+package com.learning.core.day2;
+import java.util.Scanner;
+
+public class D02P08 {
+	public static void main ( String args[]) {
+		Scanner scanner =new Scanner(System.in);
+		int l=scanner.nextInt();
+		int r=scanner.nextInt();
+		int k=l;
+		int n=0;
+		if(0<=l&&l<=r) {
+			while(k<=r) {
+				n+=k;
+				k+=1;
+				
+			}
+				
+		}
+		System.out.println("Sum of"+" "+ n);
+		
+	}
+
+}
---
+++ src/com/learning/core/day2/D02P09.java
@@ -0,0 +1,23 @@
+package com.learning.core.day2;
+
+import java.util.Scanner;
+
+public class D02P09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int n = sc.nextInt();
+
+        System.out.println("Cubes of numbers up to " + n + ":");
+        displayCubes(n);
+    }
+
+    public static void displayCubes(int n) {
+        for (int i = 1; i <= n; i++) {
+            int cube = i * i * i;
+            System.out.println(i + "^3 = " + cube);
+        }
+    }
+}
+
